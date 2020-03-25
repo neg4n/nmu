4602f386ee79fc0b30e75e2588282fcdf303c061
neg4n <nullopt@riseup.net>
2020-03-25 04:49:10 +0100

    Implement rad2deg and deg2rad

--- src/nmu.hpp
+++ src/nmu.hpp
@@ -36,7 +36,38 @@
 #endif
 
 namespace nmu {
-  template <typename T> [[nodiscard]] forceinline auto dot_product( const T & a, const T & b ) noexcept {
+
+  constexpr float pi_number_f = 3.141592f;
+  // TODO: Precision
+  constexpr double pi_number = 3.14159265359;
+
+  template <typename T>[[nodiscard]] forceinline auto rad2deg( T radians ) noexcept {
+    constexpr bool is_float = std::is_same<T, float>::value;
+    constexpr bool is_double = std::is_same<T, double>::value;
+    constexpr bool is_long_double = std::is_same<T, long double>::value;
+
+    static_assert( is_float || is_double || is_long_double, "Type must be float, double or long double" );
+
+    if constexpr ( is_float )
+      return radians * 180.f / pi_number_f;
+    else if constexpr ( is_double || is_long_double )
+      return radians * 180.0 / pi_number;
+  }
+
+  template <typename T>[[nodiscard]] forceinline auto deg2rad( T degrees ) noexcept {
+    constexpr bool is_float = std::is_same<T, float>::value;
+    constexpr bool is_double = std::is_same<T, double>::value;
+    constexpr bool is_long_double = std::is_same<T, long double>::value;
+
+    static_assert( is_float || is_double || is_long_double, "Type must be float, double or long double" );
+
+    if constexpr ( is_float )
+      return degrees * pi_number_f / 180.f;
+    else if constexpr ( is_double || is_long_double )
+      return degrees * pi_number / 180.0;
+  }
+
+  template <typename T>[[nodiscard]] forceinline auto dot_product( const T & a, const T & b ) noexcept {
     constexpr bool is_2d_vector =
         std::is_same<T, vec2_t<typename _nmu_internal::type_unwrapper<T>::template param_t<0>>>::value;
 
@@ -51,7 +82,7 @@ namespace nmu {
     }
   }
 
-  template <typename T> [[nodiscard]] forceinline auto cross_product( const T & a, const T & b ) noexcept {
+  template <typename T>[[nodiscard]] forceinline auto cross_product( const T & a, const T & b ) noexcept {
     constexpr bool is_2d_vector =
         std::is_same<T, vec2_t<typename _nmu_internal::type_unwrapper<T>::template param_t<0>>>::value;
 
@@ -66,7 +97,7 @@ namespace nmu {
     }
   }
 
-  template <typename T> [[nodiscard]] forceinline auto distance( const T & a, const T & b ) noexcept {
+  template <typename T>[[nodiscard]] forceinline auto distance( const T & a, const T & b ) noexcept {
     constexpr bool is_2d_vector =
         std::is_same<T, vec2_t<typename _nmu_internal::type_unwrapper<T>::template param_t<0>>>::value;
 
@@ -87,9 +118,14 @@ namespace nmu {
 using vec2_t = nmu::vec2_t<float>;
 using vec2i_t = nmu::vec2_t<int>;
 
+#define m_pi_f nmu::pi_number_f
+#define m_pi nmu::pi_number
+
 #define dot_product nmu::dot_product
 #define cross_product nmu::cross_product
 #define calc_distance nmu::distance
+#define rad2deg nmu::rad2deg
+#define deg2rad nmu::deg2rad
 
 #endif
 
